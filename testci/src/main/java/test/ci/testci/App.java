fe8ff6c62711641b4c8f52ed6357fced65acdcfe
HARKATI Youssef <youssef.harkati@univ-lille3.fr>
2017-04-13 10:29:01 +0200

    initial commit

---
+++ testci/src/main/java/test/ci/testci/App.java
@@ -0,0 +1,14 @@
+package test.ci.testci;
+
+/**
+ * Hello world!
+ *
+ */
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
